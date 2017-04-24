29361f9b62630c4fc9a92b7707c05e8f2432c08c
cxm <1085005134@qq.com>
2017-04-24 16:35:47 +0800

    SharedPreferences

--- app/src/main/kotlin/github/cccxm/mydemo/android/data/sp/SpActivity.kt
+++ app/src/main/kotlin/github/cccxm/mydemo/android/data/sp/SpActivity.kt
@@ -1,9 +1,96 @@
 package github.cccxm.mydemo.android.data.sp
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import github.cccxm.mydemo.utils.SpSerializable
+import github.cccxm.mydemo.utils.SpString
+import github.cccxm.mydemo.utils.SpUtil
+import github.cccxm.mydemo.utils.logger
+import org.jetbrains.anko.*
+import java.util.*
 
 /**
  * Created by cxm
  * on 2017/4/16.
  */
-class SpActivity : AppCompatActivity()
+class SpActivity : AppCompatActivity() {
+
+    private val ui = SpUI()
+    private var mUserModel: SpUserBean? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ui.setContentView(this))
+
+        val user = SpUtil.register(this, SpUserBean())
+        mUserModel = user
+        ui.setName(user.userName)
+        logger(user.time)
+
+        val appBar = supportActionBar ?: return
+        appBar.title = "SP文件"
+        appBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val user = mUserModel
+        if (user != null) {
+            user.time = Date()
+            SpUtil.unRegister(user)
+            mUserModel = null
+        }
+    }
+
+    fun saveName(name: CharSequence) {
+        val user = mUserModel
+        if (user != null) {
+            user.userName = name.toString()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
+
+private class SpUserBean {
+    var userName: String by SpString
+    var time: Date by SpSerializable(Date())
+}
+
+private class SpUI : AnkoComponent<SpActivity> {
+    private lateinit var mNameEdit: EditText
+    override fun createView(ui: AnkoContext<SpActivity>): View = with(ui) {
+        linearLayout {
+            relativeLayout {
+                editText {
+                    mNameEdit = this
+                    hint = "姓名"
+                    background = null
+                }.lparams(dip(200), dip(50)) { centerVertically() }
+                button("保存") {
+                    onClick {
+                        ui.owner.saveName(mNameEdit.text)
+                    }
+                }.lparams(wrapContent, wrapContent) {
+                    alignParentRight()
+                    centerVertically()
+                }
+            }.lparams(matchParent, wrapContent) { padding = dip(10) }
+        }
+    }
+
+    fun setName(name: String) {
+        mNameEdit.setText(name)
+    }
+}
--- app/src/main/kotlin/github/cccxm/mydemo/utils/SpUtil.kt
+++ app/src/main/kotlin/github/cccxm/mydemo/utils/SpUtil.kt
@@ -1,193 +1,174 @@
-//package github.cccxm.mydemo.utils
-//
-//import android.content.Context
-//import android.content.SharedPreferences
-//import org.aspectj.lang.ProceedingJoinPoint
-//import org.aspectj.lang.annotation.Around
-//import org.aspectj.lang.annotation.Aspect
-//import org.aspectj.lang.annotation.Pointcut
-//
-///**
-// * Created by cxm
-// * on 2017/4/15.
-// */
-//
-//@Aspect
-//class SpUtil {
-//    companion object {
-//        @JvmStatic var context: Context? = null
-//        @JvmStatic private val mSpCache = HashMap<String, SharedPreferences>()
-//    }
-//
-//    /**
-//     * 获取带有注解的类
-//     */
-//    @Pointcut("within(@github.cccxm.mydemo.utils.SharedPreferenceInject *)")
-//    fun annotatedMethod() {
-//    }
-//
-//    /**
-//     * 获得所有带有注解的setInt方法
-//     */
-////    @Pointcut("annotatedMethod() && (execution(* set*(int)) || execution(* set*(java.lang.Integer)))")
-//    @Pointcut("execution(* set*(int))")
-//    fun setIntMethod() {
-//    }
-//
-//    @Around("setIntMethod()")
-//    fun setIntMethod(joinPoint: ProceedingJoinPoint): Any? {
-//        logger("setIntMethod") //TODO
-//        val args = joinPoint.args ?: return joinPoint.proceed(joinPoint.args)
-//        val any = args[0] ?: return joinPoint.proceed(joinPoint.args)
-//        if (any is Int) {
-//            val sp = getSp(joinPoint)
-//            sp.edit().putInt(joinPoint.signature.name, any).apply()
-//        }
-//        return joinPoint.proceed(joinPoint.args)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的setBoolean方法
-//     */
-//    @Pointcut("annotatedMethod() && (execution(* set*(boolean)) || execution(* set*(java.lang.Boolean)))")
-//    fun setBooleanMethod() {
-//    }
-//
-//    @Around("setBooleanMethod()")
-//    fun setBoolean(joinPoint: ProceedingJoinPoint): Any? {
-//        val args = joinPoint.args ?: return joinPoint.proceed(joinPoint.args)
-//        val any = args[0] ?: return joinPoint.proceed(joinPoint.args)
-//        if (any is Boolean) {
-//            val sp = getSp(joinPoint)
-//            sp.edit().putBoolean(joinPoint.signature.name, any).apply()
-//        }
-//        return joinPoint.proceed(joinPoint.args)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的setFloat方法
-//     */
-//    @Pointcut("annotatedMethod() && (execution(* set*(float)) || execution(* set*(java.lang.Float)))")
-//    fun setFloatMethod() {
-//    }
-//
-//    @Around("setFloatMethod()")
-//    fun setFloat(joinPoint: ProceedingJoinPoint): Any? {
-//        val args = joinPoint.args ?: return joinPoint.proceed(joinPoint.args)
-//        val any = args[0] ?: return joinPoint.proceed(joinPoint.args)
-//        if (any is Float) {
-//            val sp = getSp(joinPoint)
-//            sp.edit().putFloat(joinPoint.signature.name, any).apply()
-//        }
-//        return joinPoint.proceed(joinPoint.args)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的setLong方法
-//     */
-//    @Pointcut("annotatedMethod() && (execution(* set*(long)) || execution(* set*(java.lang.Long)))")
-//    fun setLongMethod() {
-//    }
-//
-//    @Around("setLongMethod()")
-//    fun setLongMethod(joinPoint: ProceedingJoinPoint): Any? {
-//        val args = joinPoint.args ?: return joinPoint.proceed(joinPoint.args)
-//        val any = args[0] ?: return joinPoint.proceed(joinPoint.args)
-//        if (any is Long) {
-//            val sp = getSp(joinPoint)
-//            sp.edit().putLong(joinPoint.signature.name, any).apply()
-//        }
-//        return joinPoint.proceed(joinPoint.args)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的setString方法
-//     */
-//    @Pointcut("annotatedMethod() && execution(* set*(java.lang.String))")
-//    fun setStringMethod() {
-//    }
-//
-//    @Around("setStringMethod()")
-//    fun setStringMethod(joinPoint: ProceedingJoinPoint): Any? {
-//        val args = joinPoint.args ?: return joinPoint.proceed(joinPoint.args)
-//        val any = args[0] ?: return joinPoint.proceed(joinPoint.args)
-//        if (any is String) {
-//            val sp = getSp(joinPoint)
-//            sp.edit().putString(joinPoint.signature.name, any).apply()
-//        }
-//        return joinPoint.proceed(joinPoint.args)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的getBoolean方法
-//     */
-//    @Pointcut("annotatedMethod() && (execution(boolean is*()) || execution(java.lang.Boolean is*()))")
-//    fun getBoolean() {
-//    }
-//
-//    @Around("getBoolean()")
-//    fun getBoolean(joinPoint: ProceedingJoinPoint): Any? {
-//        val sp = getSp(joinPoint)
-//        return sp.getBoolean(joinPoint.signature.name, false)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的getInt方法
-//     */
-//    @Pointcut("annotatedMethod() && (execution(int get*()) || execution(java.lang.Integer get*()))")
-//    fun getInt() {
-//    }
-//
-//    @Around("getInt()")
-//    fun getInt(joinPoint: ProceedingJoinPoint): Any? {
-//        val sp = getSp(joinPoint)
-//        return sp.getInt(joinPoint.signature.name, 0)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的getFloat方法
-//     */
-//    @Pointcut("annotatedMethod() && (execution(float get*()) || execution(java.lang.Float get*()))")
-//    fun getFloat() {
-//    }
-//
-//    @Around("getFloat()")
-//    fun getFloat(joinPoint: ProceedingJoinPoint): Any? {
-//        val sp = getSp(joinPoint)
-//        return sp.getFloat(joinPoint.signature.name, 0f)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的getLong方法
-//     */
-//    @Pointcut("annotatedMethod() && (execution(long get*()) || execution(java.lang.Long get*()))")
-//    fun getLong() {
-//    }
-//
-//    @Around("getLong()")
-//    fun getLong(joinPoint: ProceedingJoinPoint): Any? {
-//        val sp = getSp(joinPoint)
-//        return sp.getLong(joinPoint.signature.name, 0)
-//    }
-//
-//    /**
-//     * 获得所有带有注解的getString方法
-//     */
-//    @Pointcut("annotatedMethod() && execution(java.lang.String get*())")
-//    fun getString() {
-//    }
-//
-//    @Around("getString()")
-//    fun getString(joinPoint: ProceedingJoinPoint): Any? {
-//        val sp = getSp(joinPoint)
-//        return sp.getString(joinPoint.signature.name, "")
-//    }
-//
-//    private fun getSp(joinPoint: ProceedingJoinPoint): SharedPreferences {
-//        val ctx = context ?: throw NullPointerException("context must initialize")
-//        val name = joinPoint.target.javaClass.name
-//        val sharedPreferences = mSpCache[name] ?: ctx.getSharedPreferences(name, Context.MODE_PRIVATE)
-//        mSpCache[name] = sharedPreferences
-//        return sharedPreferences
-//    }
-//}
+package github.cccxm.mydemo.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Base64
+import java.io.*
+import kotlin.reflect.KProperty
+
+private val `SpUtil$$mCache` = HashMap<Any, SharedPreferences>()
+
+object SpUtil {
+
+    /**
+     * 注册SharedPreference映射对象
+     */
+    fun <T : Any> register(context: Context, any: T): T {
+        val preferences = `SpUtil$$mCache`[any]
+        if (preferences != null) {
+            throw RuntimeException("this object already register , don't register repeat")
+        }
+        val name = any.javaClass.name
+        val sp = context.applicationContext.getSharedPreferences(name, Context.MODE_PRIVATE)
+        `SpUtil$$mCache`[any] = sp
+        return any
+    }
+
+    /**
+     * 使用完成之后应该在合适的位置取消注册，以释放内存，防止内存泄漏
+     */
+    fun unRegister(any: Any) {
+        `SpUtil$$mCache`.remove(any)
+    }
+}
+
+/**
+ * Int 类型的映射委托
+ */
+object SpInt {
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Int {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        return preferences.getInt(property.name, 0)
+    }
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: Int) {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        preferences.edit().putInt(property.name, value).apply()
+    }
+}
+
+/**
+ * Boolean 类型的映射委托
+ */
+object SpBoolean {
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Boolean {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        return preferences.getBoolean(property.name, false)
+    }
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: Boolean) {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        preferences.edit().putBoolean(property.name, value).apply()
+    }
+}
+
+/**
+ * Float 类型的映射委托
+ */
+object SpFloat {
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Float {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        return preferences.getFloat(property.name, 0f)
+    }
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: Float) {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        preferences.edit().putFloat(property.name, value).apply()
+    }
+}
+
+/**
+ * Long 类型的映射委托
+ */
+object SpLong {
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Long {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        return preferences.getLong(property.name, 0L)
+    }
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: Long) {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        preferences.edit().putLong(property.name, value).apply()
+    }
+}
+
+/**
+ * String 类型的映射委托
+ */
+object SpString {
+    operator fun getValue(thisRef: Any, property: KProperty<*>): String {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        return preferences.getString(property.name, "")
+    }
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: String) {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        preferences.edit().putString(property.name, value).apply()
+    }
+}
+
+/**
+ * StringSet 类型的映射委托
+ */
+object SpStringSet {
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Set<String> {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        return preferences.getStringSet(property.name, HashSet<String>())
+    }
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: Set<String>) {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        preferences.edit().putStringSet(property.name, value).apply()
+    }
+}
+
+/**
+ * Serializable 类型的映射委托
+ *
+ * 使用该委托对象时，需要传入一个默认值
+ */
+@Suppress("UNCHECKED_CAST")
+class SpSerializable<T : Serializable>(private val default: T) {
+    operator fun getValue(thisRef: Any, property: KProperty<*>): T {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        val string = preferences.getString(property.name, null) ?: return default
+        var objectBuffer: ObjectInputStream? = null
+        try {
+            val bytes = Base64.decode(string, Base64.DEFAULT)
+            val buffer = ByteArrayInputStream(bytes)
+            objectBuffer = ObjectInputStream(buffer)
+            val readObject = objectBuffer.readObject()
+            return readObject as T
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            if (objectBuffer != null)
+                try {
+                    objectBuffer.close()
+                } catch (e: Exception) {
+                }
+        }
+        return default
+    }
+
+    operator fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+        val preferences = `SpUtil$$mCache`[thisRef] ?: throw RuntimeException("this object do not register")
+        var objectBuffer: ObjectOutputStream? = null
+        try {
+            val buffer = ByteArrayOutputStream()
+            objectBuffer = ObjectOutputStream(buffer)
+            objectBuffer.writeObject(value)
+            val bytes = buffer.toByteArray()
+            val code = Base64.encode(bytes, Base64.DEFAULT)
+            preferences.edit().putString(property.name, String(code)).apply()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            if (objectBuffer != null)
+                try {
+                    objectBuffer.close()
+                } catch (e: Exception) {
+                }
+        }
+    }
+}
