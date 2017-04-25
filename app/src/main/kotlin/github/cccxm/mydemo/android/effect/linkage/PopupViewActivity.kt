1559f7aa1c14675209d0990f0da160180035c0e8
cxm <1085005134@qq.com>
2017-04-25 20:55:54 +0800

    dragView

--- app/src/main/kotlin/github/cccxm/mydemo/android/effect/linkage/PopupViewActivity.kt
+++ app/src/main/kotlin/github/cccxm/mydemo/android/effect/linkage/PopupViewActivity.kt
@@ -1,74 +1,98 @@
 package github.cccxm.mydemo.android.effect.linkage
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.support.v4.view.NestedScrollingParent
-import android.support.v4.view.ViewCompat
+import android.support.design.widget.CoordinatorLayout
+import android.support.v4.widget.NestedScrollView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.AttributeSet
+import android.view.MenuItem
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.Scroller
+import github.cccxm.mydemo.utils.simpleCardAdapter
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.nestedScrollView
 
 /**
  * Created by cxm
  * on 2017/4/8.
  */
-class PopupViewActivity : AppCompatActivity() {
+class PopupViewActivity
+    : AppCompatActivity() {
+
+    private val ui = PopupViewUI()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(ui.setContentView(this))
+
+        val appBar = supportActionBar ?: return
+        appBar.title = "可拖拽布局"
+        appBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
-class PopupLayout : LinearLayout, NestedScrollingParent {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+private class PopupViewUI
+    : AnkoComponent<PopupViewActivity> {
 
-    private val mScroller = Scroller(context)//控制View滑动
-
-    /**
-     * 该方法决定是否接受子控件的滑动参数
-     */
-    override fun onStartNestedScroll(child: View, target: View, nestedScrollAxes: Int): Boolean {
-        return (nestedScrollAxes and ViewCompat.SCROLL_AXIS_VERTICAL) != 0 //接受所有垂直滑动的子控件状态
-    }
-
-    /**
-     * 如果[onStartNestedScroll]方法返回true，则此方法会在子控件滑动前接受子控件的滑动参数
-     *
-     * @param target 被检测滑动状态的子控件
-     * @param dx 预计该控件在x轴方向的滑动距离
-     * @param dy 预计该控件在y轴方向的滑动距离
-     * @param consumed 实际消费距离的接收数组，在其中指定需要[target]滑动的实际距离 {x,y}
-     */
-    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray) {
-        if (dy > 0) { //向上滑动
-            consumed[1] = dy //消费滑动
-        } else {
-            if (ViewCompat.canScrollVertically(target, -1)) {//当前子View内部是否还可以向下滑动，如果不可以，则Parent消费此事件
-                consumed[1] = dy
+    override fun createView(ui: AnkoContext<PopupViewActivity>): View = with(ui) {
+        coordinatorLayout {
+            view {
+                backgroundColor = Color.TRANSPARENT
+            }.lparams(matchParent, wrapContent) {
+                behavior = PopupViewBehavior(context)
             }
+
+            nestedScrollView {
+                recyclerView {
+                    layoutManager = LinearLayoutManager(context)
+                    simpleCardAdapter {
+                        for (i in 0..50)
+                            item("Item $i")
+                    }
+                }
+            }.lparams(matchParent, matchParent)
         }
     }
+}
+
+private class PopupViewBehavior(context: Context?, attrs: AttributeSet? = null)
+    : CoordinatorLayout.Behavior<View>(context, attrs) {
 
     /**
-     * @param velocityX 水平方向滑动速度（每秒移动的像素数）
-     * @param velocityY 水平方向滑动速度（每秒移动的像素数）
-     * @param consumed 子控件是否消费了此次事件
-     * @return 父控件是否消费此次事件，如果返回true则子控件将不再接收到事件
+     * 该方法用来确定事件源和消费者之间的关系，如果返回true，则该事件源发出的事件将传递给此消费者
+     *
+     * @param parent     CoordinatorLayout
+     * @param child      该对象是被绑定对象，也是事件消费者对象
+     * @param dependency 该对象是被依赖对象，也是事件生产者对象
+     * @return true 绑定
      */
-    override fun onNestedFling(target: View, velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
-        if (scrollY >= 0) return false
-        mScroller.fling(0, scrollY, 0, velocityY.toInt(), 0, 0, 0, 0)
-        invalidate()
-        return true
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: View, dependency: View): Boolean {
+        val b = dependency is NestedScrollView
+        if (b) {
+            val height = parent.height
+            val childParams = child.layoutParams as CoordinatorLayout.LayoutParams
+            childParams.height = height / 2
+            child.layoutParams = childParams
+
+            val dependencyParams = dependency.layoutParams as CoordinatorLayout.LayoutParams
+            dependencyParams.topMargin = height / 2
+            dependency.layoutParams = dependencyParams
+        }
+        return b
     }
 
-    override fun computeScroll() {
-        if (mScroller.computeScrollOffset()) {
-            scrollTo(0, mScroller.currY)
-            invalidate()
-        }
-    }
 }
