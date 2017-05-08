c2f1b869a676f88a1785f9f514c3783750a4ecf3
陈小默 <cccxm@users.noreply.github.com>
2017-05-08 23:39:42 +0800

    Update ConditionUtil.kt

--- app/src/main/kotlin/github/cccxm/condition/api/ConditionUtil.kt
+++ app/src/main/kotlin/github/cccxm/condition/api/ConditionUtil.kt
@@ -1,7 +1,7 @@
-package com.github.cccxm.condition.api
+package github.cccxm.condition.api
 
-import com.github.cccxm.condition.annotation.Condition
-import com.github.cccxm.condition.annotation.Conditions
+import github.cccxm.condition.annotation.Condition
+import github.cccxm.condition.annotation.Conditions
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
