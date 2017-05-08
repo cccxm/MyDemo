b4883f3db97e72df86a4d1b5f1414440df335991
cxm <1085005134@qq.com>
2017-05-08 18:11:08 +0800

    barrage

--- app/src/main/kotlin/github/cccxm/mydemo/android/layout/barrage/BarrageSimpleActivity.kt
+++ app/src/main/kotlin/github/cccxm/mydemo/android/layout/barrage/BarrageSimpleActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import github.cccxm.mydemo.R
-import github.cccxm.mydemo.view.BarrageItem
-import kotlinx.android.synthetic.main.activity_barrage_simple.*
-import java.util.*
 
 /**
  * Created by cxm
@@ -17,24 +14,6 @@ class BarrageSimpleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barrage_simple)
-
-        supportActionBar?.apply {
-            title = "弹幕"
-            setDisplayHomeAsUpEnabled(true)
-        }
-        val items = LinkedList<BarrageItem>()
-        for (i in 0..30) items.add(BarrageSimpleItem())
-        mBarrageLayout.prepare(items)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mBarrageLayout.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mBarrageLayout.onStop()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -46,8 +25,4 @@ class BarrageSimpleActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-}
-
-private class BarrageSimpleItem : BarrageItem() {
-    override fun getText(): String = "吾王剑之所指，吾民心之所向"
 }
--- app/src/main/kotlin/github/cccxm/mydemo/view/BarrageLayout.kt
+++ app/src/main/kotlin/github/cccxm/mydemo/view/BarrageLayout.kt
@@ -1,14 +1,14 @@
 package github.cccxm.mydemo.view
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.TranslateAnimation
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -28,148 +28,197 @@ class BarrageLayout : RelativeLayout {
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     /**
+     * 发射间隔
+     */
+    var rate = 1000L
+    /**
      * 一个弹幕在屏幕上停留的时间，单位ms
      */
-    var mDuration = 300L
-    var speed = 10000L
+    var speed = 8000L
         set(value) {
             if (value in 1000 until 64000) {
                 field = value
-                mDuration = field / 5
             }
         }
-    var maxLine = 10
-        set(value) {
-            if (value in 1..256) field = value
-        }
-
+    private val maxLine by lazy { height / dip(40) }
+    private val mPositions by lazy { IntArray(maxLine) { it * (height - 50) / maxLine + 50 } }
+    private val mVPosition by lazy { IntArray(maxLine) { 0 } }
     private val mItems = LinkedList<BarrageItem>()
+    private val mTempItems = LinkedList<BarrageItem>()
     private var isStart = false
     private var mSubscribe: Disposable? = null
+    private var mOrignChildCount = 0
 
-    fun prepare(items: List<BarrageItem>) {
-        mItems.clear()
-        mItems.addAll(items)
+    /**
+     * 获取空闲的行数，没有时候返回-1
+     */
+    private fun getIndex(): Int {
+        for (i in mVPosition.indices) mVPosition[i]--
+        return mVPosition.indexOfFirst { it <= 0 }
+    }
+
+    fun addItem(item: BarrageItem) {
+        mTempItems.add(item)
+        mItems.add(item)
     }
 
     fun addItems(items: List<BarrageItem>) {
-        onStop()
         mItems.addAll(0, items)
-        onStart()
     }
 
-    fun onStart() {
-        onStop()
+    fun onCreate() {
+        mOrignChildCount = childCount
+        onDestroy()
         isStart = true
-        mSubscribe = Observable.create<BarrageItem> { subscribe ->
-            val items = LinkedList<BarrageItem>()
-            items.addAll(mItems)
+        mSubscribe = Observable.create<BarrageIndex> { subscribe ->
             var loop = true
             while (loop) {
+                val items = LinkedList<BarrageItem>()
+                items.addAll(mItems)
                 items.forEach {
                     if (!isStart) {
                         loop = false
                         return@forEach
                     }
-
-                    subscribe.onNext(it)
+                    var pos = getIndex()
+                    while (pos == -1) {
+                        try {
+                            Thread.sleep(buildTime())
+                            pos = getIndex()
+                        } catch (e: InterruptedException) {
+                            loop = false
+                            return@forEach
+                        }
+                    }
+                    if (mTempItems.isNotEmpty()) {
+                        while (mTempItems.isNotEmpty()) {
+                            val remove = mTempItems.remove()
+                            mVPosition[pos] = (remove.getText().length / 3) + 3
+                            subscribe.onNext(BarrageIndex(remove, pos))
+                            pos = getIndex()
+                            while (pos == -1) {
+                                try {
+                                    Thread.sleep(buildTime())
+                                    pos = getIndex()
+                                } catch (e: InterruptedException) {
+                                    loop = false
+                                    return@forEach
+                                }
+                            }
+                        }
+                    }
+                    mVPosition[pos] = (it.getText().length / 3) + 3
+                    subscribe.onNext(BarrageIndex(it, pos))
                     try {
-                        Thread.sleep(mDuration)
+                        Thread.sleep(buildTime())
                     } catch (e: InterruptedException) {
                         loop = false
                         return@forEach
                     }
                 }
+                try {
+                    Thread.sleep(speed / 2)
+                    for (i in mVPosition.indices) mVPosition[i] = 0
+                } catch (e: InterruptedException) {
+                    loop = false
+                }
             }
             subscribe.onComplete()
         }.subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    if (isStart)
-                        showBarrageItem(generateBarrageView(it))
-                }, { }, { })
+                .subscribe({ if (isStart) showBarrageItem(generateBarrageView(it.item), it.pos) }, { }, { })
     }
 
-    fun onStop() {
+    private fun buildTime(): Long {
+        val r = speed / (maxLine * 5)
+        return if (rate > 0) r else (Math.random() * r).toLong() + r
+    }
+
+    fun onDestroy() {
         isStart = false
         val sub = mSubscribe ?: return
         if (!sub.isDisposed) sub.dispose()
     }
 
-    private fun showBarrageItem(view: View) {
-        val start = right - left - paddingLeft
+    private fun showBarrageItem(view: View, pos: Int) {
+        checkChildCount()
         val params = LayoutParams(wrapContent, wrapContent)
         params.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-        params.topMargin = (Math.random() * height - 20).toInt()
+        params.topMargin = mPositions[pos]
         addView(view, params)
-        val anim = generateTranslateAnim(start.toFloat(), width.toFloat())
-        anim.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationRepeat(animation: Animation?) {}
-
-            override fun onAnimationEnd(animation: Animation?) {
-                view.clearAnimation()
-                removeView(view)
-            }
-
-            override fun onAnimationStart(animation: Animation?) {}
-        })
-        view.startAnimation(anim)
+        startAnimation(view, width.toFloat(), width.toFloat())
     }
 
-    private fun generateBarrageView(item: BarrageItem): View = with(context) {
-        linearLayout {
-            if (item.getBackgroundResource() != -1)
-                backgroundResource = item.getBackgroundResource()
-            else backgroundColor = item.getBackgroundColor()
-            orientation = LinearLayout.HORIZONTAL
-            gravity = Gravity.CENTER_VERTICAL
-            onClick { item.onBarrageClick() }
+    private fun checkChildCount() {
+        if (childCount - mOrignChildCount > 30) {
+            (mOrignChildCount until childCount).map(this::getChildAt)
+                    .forEach {
+                        it.clearAnimation()
+                        removeView(it)
+                    }
+        }
+    }
 
-            if (item.avatarEnable()) {
-                imageView {
-                    item.inflateAvatar(this)
-                    onClick { item.onAvatarClick() }
-                }.lparams(wrapContent, matchParent) {
-                    padding = dip(3)
-                    rightMargin = dip(3)
-                }
-            }
+    @SuppressLint("InflateParams") private fun generateBarrageView(item: BarrageItem): View {
+        return with(context) {
+            linearLayout {
+                linearLayout {
+                    gravity = Gravity.CENTER_VERTICAL
+                    padding = dip(5)
+                    if (item.getBackgroundResource() != -1)
+                        backgroundResource = item.getBackgroundResource()
+                    else backgroundColor = item.getBackgroundColor()
+                    onClick { item.onClick() }
+                    if (item.avatarEnable()) {
+                        imageView {
+                            item.inflateAvatar(this)
+                        }.lparams(dip(20), dip(20))
+                    }
 
-            textView(item.getText()) {
-                textColor = item.getTextColor()
-                textSize = item.getTextSize()
-                onClick { item.onTextClick() }
-            }.lparams(matchParent, wrapContent) {
-                rightMargin = dip(5)
+                    textView(item.getText()) {
+                        maxLines = 1
+                        textColor = item.getTextColor()
+                    }.lparams(wrapContent, wrapContent) {
+                        leftMargin = dip(5)
+                        rightMargin = dip(3)
+                    }
+                }.lparams(wrapContent, wrapContent)
             }
         }
     }
 
-    private fun generateTranslateAnim(start: Float, barrageWidth: Float): TranslateAnimation {
-        val anim = TranslateAnimation(start, -barrageWidth, 0f, 0f)
-        anim.duration = speed
-//        anim.interpolator = AccelerateDecelerateInterpolator()
-        anim.fillAfter = true
-        return anim
+    private fun startAnimation(view: View, start: Float, barrageWidth: Float) {
+        val animator = ObjectAnimator.ofFloat(view, "translationX", start, -barrageWidth).setDuration(speed)
+        animator.setInterpolator { it }
+        animator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {}
+            override fun onAnimationCancel(animation: Animator?) {}
+            override fun onAnimationStart(animation: Animator?) {}
+            override fun onAnimationEnd(animation: Animator?) {
+                removeView(view)
+            }
+        })
+        animator.start()
     }
 
-//    fun getTextSize(tv: TextView, text: String): Rect {
-//        val bounds = Rect()
-//        val paint = tv.paint
-//        paint.getTextBounds(text, 0, text.length, bounds)
-//        return bounds
-//    }
+    private data class BarrageIndex(val item: BarrageItem, val pos: Int)
 }
 
-abstract class BarrageItem {
-    open fun getTextColor() = Color.WHITE
-    open fun getTextSize() = 16f
-    open fun getBackgroundColor() = Color.TRANSPARENT
-    open fun getBackgroundResource(): Int = -1
-    open fun avatarEnable() = false
-    open fun inflateAvatar(avatar: ImageView) {}
-    open fun onBarrageClick() {}
-    open fun onAvatarClick() {}
-    open fun onTextClick() {}
-    abstract fun getText(): String
+interface BarrageItem {
+    fun getTextColor(): Int
+    fun getBackgroundColor(): Int
+    fun getBackgroundResource(): Int
+    fun avatarEnable(): Boolean
+    fun inflateAvatar(avatar: ImageView)
+    fun onClick()
+    fun getText(): String
+}
+
+abstract class AbsBarrageItem : BarrageItem {
+    override fun getTextColor() = Color.WHITE
+    override fun getBackgroundColor() = Color.TRANSPARENT
+    override fun getBackgroundResource(): Int = -1
+    override fun avatarEnable() = false
+    override fun inflateAvatar(avatar: ImageView) {}
+    override fun onClick() {}
 }
